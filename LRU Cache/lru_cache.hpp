5d2784efb744c32efa5adc4efe56caa6d675ef4e
Rishit Chaudhary <rishit.c@live.com>
2021-04-29 21:33:44 +0530

    Cleaned up the code and added cache look-up logic

--- LRU Cache/lru_cache.hpp
+++ LRU Cache/lru_cache.hpp
@@ -1,55 +1,41 @@
 #include <unordered_map>
 #include <functional>
+#include <cstddef>
 
+#include "lru_utility.hpp"
 
 #ifndef LRU_CACHE_HPP
 #define LRU_CACHE_HPP
 
 
-namespace Internal_LRU
-{
-    auto lazyHasher = [](size_t hash, auto&&... values)
-    {
-        auto lazyCombiner = [&hash](auto&& val)
-        {
-            hash ^= std::hash<std::decay_t<decltype(val)>>{}(val) + 0Xeeffddcc + (hash << 5) + (hash >> 3);
-        };
-        (lazyCombiner(std::forward<decltype(values)>(values)), ...);
-        return hash;
-    };
-
-    struct tuple_hasher
-    {
-        template<typename... T>
-        std::size_t operator()(const std::tuple<T...>& tup)
-        {
-            size_t hash = 0;
-            std::apply(lazyHasher, std::tuple_cat(std::tuple(0), tup));
-            return hash;
-        }
-    };
-}
-
-
-
-
 template<typename R, typename... Args>
 class lru_cache{
     public:
-    lru_cache(std::function<R(Args...)> f) : func_{f} {
+    lru_cache(std::function<R(Args...)> f) : func_{f}, cache{} {
         static_assert(!std::is_void<R>::value,
                   "Return type of the function-to-wrap must not be void!");
     }
-    void operator () (Args...) {
-        return;
+    R operator () (Args... arg_list) {
+        auto tuple_ele = std::make_tuple(arg_list...);
+        if (cache.find(tuple_ele) != std::end(cache)) {
+            std::cout << "Cache Hit!" << std::endl;
+            ++cache[tuple_ele].second;
+            return cache[tuple_ele].first;
+        }
+
+        cache[tuple_ele] = Values{func_(arg_list...), 1};
+        std::cout << "Cache Miss" << std::endl;
+        std::cout << "Value added to cache" << std::endl;
+        return cache[tuple_ele].first;
     }
 
     private:
-    using counter_t = size_t
+    using counter_t = size_t;
     using Arguments = std::tuple<Args...>;
     using Values = std::pair<R, counter_t>;
     using lru_cache_t = std::unordered_map<Arguments,Values,Internal_LRU::tuple_hasher>;
     std::function<R(Args...)> func_;
+    lru_cache_t cache;
 };
 
 
