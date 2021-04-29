b2dee6e4ac9ff05e5ee26c395e5748f911bef042
Rishit Chaudhary <rishit.c@live.com>
2021-04-29 21:34:12 +0530

    Moved utility code to a separate file

---
+++ LRU Cache/lru_utility.hpp
@@ -0,0 +1,28 @@
+#ifndef LRU_UTILITY_HPP
+#define LRU_UTILITY_HPP
+
+namespace Internal_LRU
+{
+    auto lazyHasher = [](size_t hash, auto&&... values)
+    {
+        auto lazyCombiner = [&hash](auto&& val)
+        {
+            hash ^= std::hash<std::decay_t<decltype(val)>>{}(val) + 0Xeeffddcc + (hash << 5) + (hash >> 3);
+        };
+        (lazyCombiner(std::forward<decltype(values)>(values)), ...);
+        return hash;
+    };
+
+    struct tuple_hasher
+    {
+        template<typename... T>
+        std::size_t operator()(const std::tuple<T...>& tup) const
+        {
+            size_t hash = 0;
+            std::apply(lazyHasher, std::tuple_cat(std::tuple(0), tup));
+            return hash;
+        }
+    };
+}
+
+#endif
