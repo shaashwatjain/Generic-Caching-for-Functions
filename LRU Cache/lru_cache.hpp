32bbd73f882a9e2912ee254828657f95f9f6f055
Rishit Chaudhary <rishit.c@live.com>
2021-04-29 19:29:47 +0530

    static_assert to avoid functions with void return type

--- LRU Cache/lru_cache.hpp
+++ LRU Cache/lru_cache.hpp
@@ -6,15 +6,20 @@
 #define LRU_CACHE_HPP
 
 
-template<typename R, typename ...Args>
+template<typename R, typename... Args>
 class lru_cache{
     public:
-    lru_cache(std::function<R(Args...)> f) : func_{f} {}
+    lru_cache(std::function<R(Args...)> f) : func_{f} {
+        static_assert(!std::is_void<R>::value,
+                  "Return type of the function-to-wrap must not be void!");
+    }
     void operator () (Args...) {
         return;
     }
 
     private:
+    using Arguments = std::tuple<Args...>;
+
     std::function<R(Args...)> func_;
 };
 
