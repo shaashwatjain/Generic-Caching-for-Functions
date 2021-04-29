57131166d3f5a0296fd1b3a049fa80addd462e26
Rishit Chaudhary <rishit.c@live.com>
2021-04-29 20:31:39 +0530

    Added Internal LRU utility functions

--- LRU Cache/lru_cache.hpp
+++ LRU Cache/lru_cache.hpp
@@ -6,6 +6,33 @@
 #define LRU_CACHE_HPP
 
 
+namespace Internal_LRU
+{
+    auto lazyHasher = [](size_t hash, auto&&... values)
+    {
+        auto lazyCombiner = [&hash](auto&& val)
+        {
+            hash ^= std::hash<std::decay_t<decltype(val)>>{}(val) + 0Xeeffddcc + (hash << 5) + (hash >> 3);
+        };
+        (lazyCombiner(std::forward<decltype(values)>(values)), ...);
+        return hash;
+    };
+
+    struct tuple_hasher
+    {
+        template<typename... T>
+        std::size_t operator()(const std::tuple<T...>& tup)
+        {
+            size_t hash = 0;
+            std::apply(lazyHasher, std::tuple_cat(std::tuple(0), tup));
+            return hash;
+        }
+    };
+}
+
+
+
+
 template<typename R, typename... Args>
 class lru_cache{
     public:
@@ -18,8 +45,10 @@ class lru_cache{
     }
 
     private:
+    using counter_t = size_t
     using Arguments = std::tuple<Args...>;
-
+    using Values = std::pair<R, counter_t>;
+    using lru_cache_t = std::unordered_map<Arguments,Values,Internal_LRU::tuple_hasher>;
     std::function<R(Args...)> func_;
 };
 
