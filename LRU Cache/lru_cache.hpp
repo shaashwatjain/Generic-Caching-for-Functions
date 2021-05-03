50cc064f2ec5f0c2c44b5edd8060c7cae186f1c3
Rishit Chaudhary <rishit.c@live.com>
2021-05-03 22:07:34 +0530

    Cleared class declaration and implementation

--- LRU Cache/lru_cache.hpp
+++ LRU Cache/lru_cache.hpp
@@ -6,11 +6,11 @@
 
 template<typename R, typename ...Args>
 class lru_cache {
-    // 
+    // Creating type alias for complex types
     using Arguments = std::tuple<Args...>;
     using Values = std::pair<R, typename std::list<Arguments>::iterator>;
 
-    //    
+    // Creating type alias for complex types
     using cache_size_t = std::optional<std::size_t>;
     using cache_t = std::unordered_map<Arguments, Values, internal_cache::tuple_hasher>;
     using list_it_t = std::list<Arguments>::iterator;
@@ -23,38 +23,9 @@ class lru_cache {
             if (cache_size.has_value())
                 cache_.reserve(cache_size.value());
         }
-        R operator () (Args... arg_list)
-        {
-            const auto tuple_ele = std::make_tuple(arg_list...);
 
-            if (cache_.find(tuple_ele) != std::end(cache_)) {
-                list_it_t &list_it_ = cache_[tuple_ele].second;
-                std::cout << "Cache Hit!" << std::endl;
-                usage_tracker_.erase(list_it_);
-                usage_tracker_.push_back(tuple_ele);
-                list_it_ = std::end(usage_tracker_);
-            }
-            else {
-                // Eviction code
-                if (cache_size_.has_value() && cache_.size() == cache_size_.value()) {
-                    list_const_it_t lru_const_it_ = std::begin(usage_tracker_);
-                    cache_.erase(*lru_const_it_);  // Eviction by Key
-                    usage_tracker_.erase(lru_const_it_);  // Eviction by Iterator
-                }
-
-                usage_tracker_.push_back(tuple_ele);
-                cache_[tuple_ele] = Values{func_(arg_list...), std::end(usage_tracker_)};
-                std::cout << "Cache Miss" << std::endl;
-                std::cout << "Value added to cache" << std::endl;
-            }
-
-            return cache_[tuple_ele].first;
-        }
-        void flush_cache()
-        {
-            cache_.clear();
-            usage_tracker_.clear();
-        }
+        R operator () (Args... arg_list);
+        void flush_cache();
 
 
     private:
@@ -63,3 +34,40 @@ class lru_cache {
         cache_t cache_;
         std::list<Arguments> usage_tracker_;
 };
+
+
+template<typename R, typename ...Args>
+R lru_cache<R, Args...>::operator () (Args... arg_list)
+{
+    const auto tuple_ele = std::make_tuple(arg_list...);
+
+    if (cache_.find(tuple_ele) != std::end(cache_)) {
+        list_it_t &list_it_ = cache_[tuple_ele].second;
+        std::cout << "Cache Hit!" << std::endl;
+        usage_tracker_.erase(list_it_);
+        usage_tracker_.push_back(tuple_ele);
+        list_it_ = std::end(usage_tracker_);
+    }
+    else {
+        // Eviction code
+        if (cache_size_.has_value() && cache_.size() == cache_size_.value()) {
+            list_const_it_t lru_const_it_ = std::begin(usage_tracker_);
+            cache_.erase(*lru_const_it_);  // Eviction by Key
+            usage_tracker_.erase(lru_const_it_);  // Eviction by Iterator
+        }
+
+        usage_tracker_.push_back(tuple_ele);
+        cache_[tuple_ele] = Values{func_(arg_list...), std::end(usage_tracker_)};
+        std::cout << "Cache Miss" << std::endl;
+        std::cout << "Value added to cache" << std::endl;
+    }
+
+    return cache_[tuple_ele].first;
+}
+
+template<typename R, typename ...Args>
+void lru_cache<R, Args...>::flush_cache()
+{
+    cache_.clear();  // Clear the cache
+    usage_tracker_.clear();  // Clear the bookkeeping
+}
