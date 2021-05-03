33c5bf1e30e41c32c2b6b2d8c449c5b73afe2ab1
Rishit Chaudhary <rishit.c@live.com>
2021-05-03 19:17:59 +0530

    Fixed code format and added options

--- LRU Cache/lru_cache.hpp
+++ LRU Cache/lru_cache.hpp
@@ -10,19 +10,23 @@
 
 namespace Policy
 {
-    struct LRU_CACHE {
+    struct LRU_CACHE
+    {
         enum {type = 0};
     };
 
-    struct MRU_CACHE {
+    struct MRU_CACHE 
+    {
         enum {type = 1};
     };
 
-    struct LFU_CACHE {
+    struct LFU_CACHE
+    {
         enum {type = 2};
     };
 
-    struct MFU_CACHE {
+    struct MFU_CACHE
+    {
         enum {type = 3};
     };
 
@@ -31,8 +35,14 @@ namespace Policy
 template<typename R, typename ...Args>
 class lru_cache {
     public:
-        explicit lru_cache(std::function<R(Args...)> f) : cache_{}, func_{f} {}
-        R operator () (Args... arg_list) {
+        explicit lru_cache(std::function<R(Args...)> f, std::optional<int> cache_size = std::nullopt) :
+            cache_{}, func_{f}, cache_size_{cache_size} 
+        {
+            if (cache_size.has_value())
+                cache_.reserve(cache_size.value());
+        }
+        R operator () (Args... arg_list)
+        {
             auto tuple_ele = std::make_tuple(arg_list...);
 
             if (cache_.find(tuple_ele) != std::end(cache_)) {
@@ -46,7 +56,8 @@ class lru_cache {
             std::cout << "Value added to cache" << std::endl;
             return cache_[tuple_ele].first;
         }
-        void flush_cache() {
+        void flush_cache()
+        {
             cache_.clear();
         }
 
@@ -55,9 +66,10 @@ class lru_cache {
         using counter_t = std::size_t;
         using Arguments = std::tuple<Args...>;
         using Values = std::pair<R, counter_t>;
-        using cache_t = std::unordered_map<Arguments, Values, Internal_LRU::tuple_hasher>;
+        using cache_t = std::unordered_map<Arguments, Values, internal_cache::tuple_hasher>;
         cache_t cache_;
         std::function<R(Args...)> func_;
+        std::optional<int> cache_size_;
 };
 
 
@@ -65,17 +77,25 @@ template<int N, typename... T>
 using static_switch = typename std::tuple_element<N, std::tuple<T...> >::type;
 
 template<typename Policy, typename R, typename ...Args>
-class my_cache {
+class my_cache
+{
     using selected_cache_t = static_switch<Policy::type, lru_cache<R, Args...>>;
     public:
-        explicit my_cache(Policy, std::function<R(Args...)> f) : cache_{f} {
+        explicit my_cache(Policy, std::function<R(Args...)> f) :
+            cache_{f}
+        {
             static_assert(!std::is_void<R>::value,
                         "Return type of the function-to-wrap must not be void!");
         }
 
-        R operator () (Args... arg_list) {};
-        void flush_cache() {};
-        ~my_cache() {};
+        R operator () (Args... arg_list)
+        {
+            return cache_(arg_list...);
+        }
+        void flush_cache()
+        {
+            cache_.flush_cache();
+        }
 
     private:
         selected_cache_t cache_;
